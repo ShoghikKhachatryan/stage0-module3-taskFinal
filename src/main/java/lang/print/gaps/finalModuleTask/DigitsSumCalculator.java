515b27fb90db89c852c7822dbe723962f7df94b0
Shoghik <shoghik.kachatryan@dci-student.org>
2023-10-13 21:43:13 +0200

    first commit

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,13 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+        int sum = number / 1000;
+        number %= 1000;
+         sum +=  number / 100 ;
+        number %= 100;
+        sum += number / 10;
+        number %= 10;
+        sum += number;
+        System.out.println(sum);
     }
 }
